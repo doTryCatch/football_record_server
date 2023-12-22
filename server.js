44fd123b7305991980a11a139f2e9f21355bf7c3
doTryCatch <rpsuper1640@gmail.com>
2023-12-22 19:27:13 +0530

    code update changes made

--- server.js
+++ server.js
@@ -4,39 +4,12 @@ const csv = require('csv-parser');
 const express=require('express')
 const app=express()
 const cors=require("cors")
-const PORT=process.env.PORT||3002
-// **********************************8
-const FootballDataModel=require("./mongoSchema")
-// const url = "mongodb+srv://Raone:roshan12@cluster0.muoib0h.mongodb.net/";
+const PORT=3001
+
+const DataModel=require("./Schema")
+const databse='/FOOTBALL_RECORD_SET'
 const url = "mongodb://127.0.0.1:27017";
-
-
-
-
-// Connect to MongoDB Atlas
-mongoose.connect(url + '/FOOTBALL_RECORD_SET', { useNewUrlParser: true, useUnifiedTopology: true });
-
-
-
-
-// *****************************888
-//supporting function 
-const sorting=(arr)=>{
-for (let i = 0; i < arr.length-1; i++) {
-   for (let j = i+1; j < arr.length; j++) {
-    if(arr[i].Win<arr[j].Win){
-        let temp=arr[i]
-        arr[i]=arr[j]
-        arr[j]=temp
-    }
-  
-    
-   }    
-}
-
-return arr
-}
-    
+mongoose.connect(url + databse,{ useNewUrlParser: true, useUnifiedTopology: true });
 
 // ******************************8
 app.use(cors())
@@ -52,30 +25,30 @@ app.route("/")
     }
     try {
        
-        let result = await FootballDataModel.find();
+        let Res = await DataModel.find();
       
 
-        if (!result || result.length === 0) {
-            console.log("empty")
+        if ( Res.length === 0) {
+        
             try {
                 await new Promise((resolve, reject) => {
                     fs.createReadStream('FootbalCSV.csv')
                         .pipe(csv())
-                        .on('data', async (row) => {
-                            row.GamesPlayed = parseInt(row.GamesPlayed);
-                            row.Win = parseInt(row.Win);
-                            row.Draw = parseInt(row.Draw);
-                            row.Loss = parseInt(row.Loss);
-                            row.GoalsFor = parseInt(row.GoalsFor);
-                            row.GoalsAgainst = parseInt(row.GoalsAgainst);
-                            row.Points = parseInt(row.Points);
-                            row.Year = parseInt(row.Year);
-                            const footballData = new FootballDataModel(row);
-                            console.log(row)
+                        .on('data', async (data) => {
+                            data.GamesPlayed = parseInt(data.GamesPlayed);
+                            data.Win = parseInt(data.Win);
+                            data.Draw = parseInt(data.Draw);
+                            data.Loss = parseInt(data.Loss);
+                            data.GoalsFor = parseInt(data.GoalsFor);
+                            data.GoalsAgainst = parseInt(data.GoalsAgainst);
+                            data.Points = parseInt(data.Points);
+                            data.Year = parseInt(data.Year);
+                            const footballData = new DataModel(data);
+                            console.log(data)
                             await footballData.save();
                         })
                         .on('end', () => {
-                            console.log('CSV file successfully processed');
+                            console.log('CSV file successfully saved');
                             resolve();
                         })
                         .on('error', (error) => {
@@ -89,40 +62,44 @@ app.route("/")
             }
         }
 
-        result = await FootballDataModel.find();
+        Res = await DataModel.find();
         if(mode=="WDL"){
-            result=result.filter((item)=> item['Year']>parseInt(input))
-            const { GamesPlayed, Win, Draw } = result.reduce(
-                (accumulator, item) => ({
-                  GamesPlayed: accumulator.GamesPlayed + item.GamesPlayed,
-                  Win: accumulator.Win + item.Win,
-                  Draw: accumulator.Draw + item.Draw,
+            Res=Res.filter((data)=> data['Year']>parseInt(input))
+            const { GamesPlayed, Win, Draw } = Res.reduce(
+                (accumulator, data) => ({
+                  GamesPlayed: accumulator.GamesPlayed + data.GamesPlayed,
+                  Win: accumulator.Win + data.Win,
+                  Draw: accumulator.Draw + data.Draw,
                 }),
                 { GamesPlayed: 0, Win: 0, Draw: 0 }
               );
             
               
-              // Assign the calculated values back to the 'result' object if needed
-              result = [{ GamesPlayed, Win, Draw ,Year:input}];
+              // Assign the calculated values back to the 'Res' object if needed
+              Res = [{ GamesPlayed, Win, Draw ,Year:input}];
 
         }else if(mode=="FTR"){
-            result=result.filter((item)=> item['Win']>parseInt(input))
-            result=sorting(result).slice(0,10)
+            Res=Res.filter((data)=> data['Win']>parseInt(input))
+            if(Res.length>10){
+                Res=Res.splice(0,10)
+            }
+            
+           
           
 
     
         }else if(mode=="AGF"){
-            result=result.filter((item)=> item['Year']===parseInt(input))
-            result = result.map((item) => ({
-                ...item._doc,
-                ...{AverageGoalFor: (item['GoalsFor'] / item['GamesPlayed']).toFixed(2)}
+            Res=Res.filter((data)=> data['Year']===parseInt(input))
+            Res = Res.map((data) => ({
+                ...data._doc,
+                ...{AverageGoalFor: (data['GoalsFor'] / data['GamesPlayed']).toFixed(2)}
               }));
         }
-       console.log(result)
+       console.log(Res)
        
-        res.status(200).json(result);
+        res.status(200).json(Res);
     } catch (error) {
-        console.error("nothing happened");
+
         res.status(500).json({ "message": "Internal server error" });
     }
 
@@ -130,9 +107,8 @@ app.route("/")
    
     const data=req.body;
     console.log(data)
-    // team, gamesPlayed,win,draw,loss,goalsFor,goalsAgainst,points,year 
     try {
-        const footballData = new FootballDataModel({
+        const footballData = new DataModel({
             "Team": data.team,
             "GamesPlayed": parseInt(data.gamesPlayed),
             "Win": parseInt(data.win),
@@ -145,28 +121,25 @@ app.route("/")
            
         });
 
-        // Save the document asynchronously using async/await
+       
         await footballData.save();
 
-        // Send a success response to the client
+       
         res.status(200).json({ message: 'newData saved successfully' });
     } catch (error) {
         console.error('Error saving document:', error);
-        // Send an error response to the client
+    
         res.status(500).json({ error: 'Internal Server Error' });
     }
 }).put(async(req,res)=>{
     const {data}=req.body;
-    const team=data.team
-    const year=data.year
-    const field=data.field
-    const newValue=data.newValue
-    console.log( data)
-
     try{
-        const result = await FootballDataModel.updateOne({Team:team,Year:parseInt(year) }, { $set: {[field]:parseInt(newValue)} });
-      console.log(result)
-      result.matchedCount > 0?  res.status(200).json({"msg":"successfully updated!!"}):res.status(200).json({"msg":"Data is not in the list. please give valid info!!"})           
+        const Res = await DataModel.updateOne({Team:data.team,Year:parseInt(data.year) }, { $set: {[data.field]:parseInt(data.newValue)} });
+     if( Res.matchedCount > 0){
+         res.status(200).json({"msg":"successfully updated!!"})
+     }else{
+        res.status(200).json({"msg":"Data is not in the list. please give valid info!!"})           
+     }
 
     
              
@@ -182,23 +155,19 @@ app.route("/")
     const {team,year}=req.body;
     console.log({team,year})
     try{
-         const result=await FootballDataModel.deleteOne({ Team:team,Year:parseInt(year)});
-         console.log(result)
-         result.deletedCount > 0?  res.status(200).json({"msg":"successfully deleted!!"})           :  res.status(200).json({"msg":"Data is not in the list. please give valid info!!"})           
+         const Res=await DataModel.deleteOne({ Team:team,Year:parseInt(year)});
+         if (Res.deletedCount>0){
+            res.status(200).json({"msg":"successfully deleted!!"})
+         }else{
+            res.status(200).json({"msg":"Data is not in the list. please give valid info!!"})   
+         }
+       
+         
     }catch(err){
         console.error(err)
     }
-   
-   
-
-    // acknowledged: true,
-    // modifiedCount: 0,
-    // upsertedId: null,
-    // upsertedCount: 0,
-    // matchedCount: 1
-
 
 })
 app.listen(PORT,()=>{
-    console.log("server is live!!")
+    console.log("server is live and ready to go!!")
 })
